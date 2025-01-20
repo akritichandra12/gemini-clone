8dc7c2f481e2cdd632e8ad665880e187ebbc718e
Akriti Chandra <akritichandra120803@gmail.com>
2025-01-20 18:00:46 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+
+import Main from './components/Main/Main'
+import Sidebar from './components/sidebar/sidebar'
+
+const App = () => {
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
+
